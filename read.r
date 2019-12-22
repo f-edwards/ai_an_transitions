bdeea8b06985efd2a3e3974e0bbc2b3b03c6685a
f-edwards <fedwards@uw.edu>
2019-12-22 15:10:20 -0800

    ff

--- read.r
+++ read.r
@@ -28,7 +28,7 @@ pop<-pop %>%
 afcars<-read_csv("./data/afcars_imputed_all_cases.csv",
                  col_types = cols(stfcid = "c")) 
 
-# ncands<-read_csv("./data/ncands_imputed.csv")
+ncands<-read_csv("./data/ncands_imputed.csv")
 
 ncands_xwalk<-read_csv("./data/ncands_xwalk.csv",
                        col_types = cols(stfcid = "c"))
@@ -40,51 +40,55 @@ state.fips<-state.fips %>%
   distinct() %>% 
   rename(staterr = abb)
 
-# ### attach stfcid
-# ncands<-ncands %>% 
-#   left_join(ncands_xwalk)
-# 
-# ### get needed first events
-# 
-# ncands<-ncands %>% 
-#   filter(!(is.na(subyr)))
-# 
-# ncands_first<-ncands %>% 
-#   select(.imp, chid, race_ethn, chage, rptdt) %>% 
-#   group_by(chid) %>% 
-#   filter(rptdt==min(rptdt)) %>% 
-#   distinct() %>% 
-#   ungroup() %>% 
-#   mutate(year = year(rptdt))
-# 
-# ncands_first_nat<-ncands_first %>% 
-#   group_by(.imp, chage, race_ethn, year) %>% 
-#   summarise(var = n()) %>% 
-#   rename(age = chage) %>% 
-#   left_join(pop) %>% 
-#   ungroup() %>% 
-#   write_csv("./data/ncands_first_inv.csv")
-# 
-# ncands_first_subst<-ncands %>% 
-#   filter(rptvictim==1) %>% 
-#   group_by(chid) %>% 
-#   filter(rptdt==min(rptdt))%>% 
-#   distinct() %>% 
-#   ungroup() %>% 
-#   mutate(year = year(rptdt))
-# 
-# ncands_first_subst_nat<-ncands_first_subst %>% 
-#   group_by(.imp, chage, race_ethn, year) %>% 
-#   summarise(var = n()) %>% 
-#   rename(age = chage) %>% 
-#   left_join(pop) %>% 
-#   ungroup() %>% 
-#   write_csv("./data/ncands_first_sub.csv")
+zz <- file("all.Rout", open = "wt")
+sink(zz)
+sink(zz, type = "message")
+### attach stfcid
+ncands<-ncands %>%
+  left_join(ncands_xwalk)
+
+### get needed first events
+
+ncands<-ncands %>%
+  filter(!(is.na(subyr)))
+
+ncands_first<-ncands %>%
+  select(.imp, chid, race_ethn, chage, rptdt) %>%
+  arrange(.imp, rptdt) %>% 
+  group_by(.imp, stfcid) %>%
+  slice(1)
+  ungroup() %>%
+  mutate(year = year(rptdt))
+
+ncands_first_nat<-ncands_first %>%
+  group_by(.imp, chage, race_ethn, year) %>%
+  summarise(var = n()) %>%
+  rename(age = chage) %>%
+  left_join(pop) %>%
+  ungroup() %>%
+  write_csv("./data/ncands_first_inv.csv")
+
+ncands_first_subst<-ncands %>%
+  filter(rptvictim==1) %>%
+  arrange(.imp, rptdt) %>% 
+  group_by(.imp, stfcid) %>%
+  slice(1) %>% 
+  ungroup() %>%
+  mutate(year = year(rptdt))
+
+ncands_first_subst_nat<-ncands_first_subst %>%
+  group_by(.imp, chage, race_ethn, year) %>%
+  summarise(var = n()) %>%
+  rename(age = chage) %>%
+  left_join(pop) %>%
+  ungroup() %>%
+  write_csv("./data/ncands_first_sub.csv")
 
 afcars_first <- afcars %>% 
-  arrange(stfcid, year, age) %>% 
-  group_by(stfcid) %>% 
-  slice(1)
+  arrange(.imp, year, age) %>% 
+  group_by(.imp, stfcid) %>% 
+  slice(1) %>% 
+  ungroup()
 
 sink("log.txt")
 print("afcars_first length")
@@ -101,7 +105,6 @@ afcars_first_nat<-afcars_first %>%
   write_csv("./data/afcars_first_fc.csv")
 
 afcars_inv<-afcars_first %>% 
-  ungroup() %>% 
   left_join(ncands_xwalk %>% 
               select(stfcid, rptdt) %>% 
               arrange(rptdt) %>% 
@@ -116,14 +119,13 @@ print(length(unique(afcars_inv$stfcid)))
 sink()
 
 afcars_inv<-afcars_inv %>% 
-  ungroup() %>% 
   mutate(rpt_year = year(rptdt)) %>% 
   filter(!(is.na(ncands_inv))) 
 
 # remove when year(rptdt)>year
 
 afcars_inv<-afcars_inv %>%
-  filter(year>=rpt_year)
+  filter(year<=rpt_year)
 
 afcars_inv_nat<-afcars_inv %>%
   group_by(.imp, age, race_ethn, year) %>%
@@ -131,60 +133,58 @@ afcars_inv_nat<-afcars_inv %>%
   left_join(pop) %>%
   ungroup() %>%
   write_csv("./data/afcars_fc_post_inv.csv")
-# 
-# afcars_tpr<-afcars %>% 
-#   filter(istpr==1) %>% 
-#   group_by(stfcid) %>% 
-#   filter(year==min(year)) %>% 
-#   ungroup() %>% 
-#   select(.imp, age, race_ethn, year) %>% 
-#   distinct()
-# 
-# afcars_tpr_nat<-afcars_tpr %>% 
-#   group_by(.imp, age, race_ethn, year) %>% 
-#   summarise(var = n()) %>% 
-#   left_join(pop) %>% 
-#   ungroup() %>% 
-#   write_csv("./data/afcars_tpr.csv")
-# 
-# afcars_icwa<-afcars %>% 
-#   filter(race_ethn=="AI/AN") %>% 
-#   mutate(icwa_compliant = case_when(
-#     curplset==2 ~ T,
-#     rf1amakn==1 ~ T,
-#     rf2amakn==1 ~ T,
-#     T ~ F)) %>% 
-#   filter(icwa_compliant == F) %>% 
-#   group_by(stfcid) %>% 
-#   filter(year==min(year)) %>% 
-#   ungroup() %>% 
-#   select(.imp, stfcid, age, race_ethn, year) %>% 
-#   distinct()
-# 
-# afcars_icwa_nat<-afcars_icwa %>% 
-#   group_by(.imp, age, race_ethn, year) %>% 
-#   summarise(var = n()) %>% 
-#   left_join(pop) %>% 
-#   ungroup() %>% 
-#   write_csv("./data/afcars_non_icwa.csv")
 
-# 
-# 
-# pop_nat<-pop %>% 
-#   filter(race_ethn=="AI/AN" | race_ethn=="White") %>% 
-#   group_by(year, age, race_ethn) %>% 
+afcars_tpr<-afcars %>%
+  filter(istpr==1) %>%
+  arrange(.imp, year) %>% 
+  group_by(.imp, stfcid) %>%
+  slice(1) %>% 
+  ungroup() 
+
+afcars_tpr_nat<-afcars_tpr %>%
+  group_by(.imp, age, race_ethn, year) %>%
+  summarise(var = n()) %>%
+  ungroup() %>%
+  left_join(pop) %>%
+  write_csv("./data/afcars_tpr.csv")
+
+afcars_icwa<-afcars %>%
+  filter(race_ethn=="AI/AN") %>%
+  mutate(icwa_compliant = case_when(
+    curplset==2 ~ T,
+    rf1amakn==1 ~ T,
+    rf2amakn==1 ~ T,
+    T ~ F)) %>%
+  filter(icwa_compliant == F) %>%
+  arrange(.imp, year) %>% 
+  group_by(.imp, stfcid) %>%
+  slice(1) %>% 
+  ungroup() 
+
+afcars_icwa_nat<-afcars_icwa %>%
+  group_by(.imp, age, race_ethn, year) %>%
+  summarise(var = n()) %>%
+  left_join(pop) %>%
+  ungroup() %>%
+  write_csv("./data/afcars_non_icwa.csv")
+
+# pop_nat<-pop %>%
+#   filter(race_ethn=="AI/AN" | race_ethn=="White") %>%
+#   group_by(year, age, race_ethn) %>%
 #   summarise(pop = sum(pop))
 # 
 # write.csv(pop_nat, "./data/pop_nat.csv", row.names=F)
 # 
-# ncands_aian<-ncands %>% 
-#   filter(race_ethn=="AI/AN" | race_ethn=="White") %>% 
-#   filter(.imp==1) %>% 
+# ncands_aian<-ncands %>%
+#   filter(race_ethn=="AI/AN" | race_ethn=="White") %>%
+#   filter(.imp==1) %>%
 #   write.csv("./data/ncands_subset.csv", row.names=F)
 # 
-# afcars_aian<-afcars %>% 
-#   filter(race_ethn=="AI/AN" | race_ethn=="White") %>% 
-#   filter(.imp==1) %>% 
+# afcars_aian<-afcars %>%
+#   filter(race_ethn=="AI/AN" | race_ethn=="White") %>%
+#   filter(.imp==1) %>%
 #   write.csv("./data/afcars_subset.csv", row.names=F)
+sink(type="message")
+sink()
 q("no")
   
